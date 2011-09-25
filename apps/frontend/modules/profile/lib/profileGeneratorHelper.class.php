a4f8ac70788b7008dc8849b018c9a2b9944ef249
Tom Doggett <nocoolnametom@gmail.com>
2011-09-24 19:05:10 -0600

    Revert a3d8ddf3515d31b085ff8f47ef39d5d48a092791^..HEAD

---
+++ apps/frontend/modules/profile/lib/profileGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * profile module helper.
+ *
+ * @package    OpenMicNight
+ * @subpackage profile
+ * @author     Tom Doggett
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class profileGeneratorHelper extends BaseProfileGeneratorHelper
+{
+}
---
+++ lib/filter/doctrine/ProfileSubredditMembershipFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * ProfileSubredditMembership filter form.
+ *
+ * @package    OpenMicNight
+ * @subpackage filter
+ * @author     Tom Doggett
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class ProfileSubredditMembershipFormFilter extends BaseProfileSubredditMembershipFormFilter
+{
+  public function configure()
+  {
+  }
+}
