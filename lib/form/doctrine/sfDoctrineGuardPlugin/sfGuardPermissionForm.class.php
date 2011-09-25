15a33045737e1dc60a6fc7b461d75d40bc749432
Tom Doggett <nocoolnametom@gmail.com>
2011-09-24 22:08:14 -0600

    Added sfDoctrineGuardPlugin.  Seems to be okay.  Will begin work now on converting it for use in project.

---
+++ lib/form/doctrine/sfDoctrineGuardPlugin/sfGuardPermissionForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * sfGuardPermission form.
+ *
+ * @package    OpenMicNight
+ * @subpackage form
+ * @author     Tom Doggett
+ * @version    SVN: $Id: sfDoctrinePluginFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class sfGuardPermissionForm extends PluginsfGuardPermissionForm
+{
+  public function configure()
+  {
+  }
+}
---
+++ lib/form/doctrine/sfDoctrineGuardPlugin/sfGuardUserForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * sfGuardUser form.
+ *
+ * @package    OpenMicNight
+ * @subpackage form
+ * @author     Tom Doggett
+ * @version    SVN: $Id: sfDoctrinePluginFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class sfGuardUserForm extends PluginsfGuardUserForm
+{
+  public function configure()
+  {
+  }
+}
