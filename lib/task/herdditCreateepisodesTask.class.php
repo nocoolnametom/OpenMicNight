aa26fbe00c64e42e3e594077418a1b87416586c0
Tom Doggett <nocoolnametom@gmail.com>
2011-09-27 23:02:57 -0600

    Added a task that creates Episodes according to the cron jobs defined for each subreddit.  It needs further testing, however, and the subquery that helps drive it seems to be throwing errors: PHP Notice:  Array to string conversion in /opt/symfony/symfony14/lib/plugins/sfDoctrinePlugin/lib/vendor/doctrine/Doctrine/Connection/Statement.php on line 252.

--- lib/task/herdditCreateepisodesTask.class.php
+++ lib/task/herdditCreateepisodesTask.class.php
@@ -2,49 +2,77 @@
 
 class herdditCreateepisodesTask extends sfBaseTask
 {
-  protected function configure()
-  {
-    // // add your own arguments here
-    $this->addArguments(array(
-        new sfCommandArgument('subreddit', sfCommandArgument::OPTIONAL, 'The name of any specific subreddit'),
-    ));
 
-    $this->addOptions(array(
-      new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name'),
-      new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
-      new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'doctrine'),
-      // add your own options here
-    ));
+    protected function configure()
+    {
+        // // add your own arguments here
+        $this->addArguments(array(
+            new sfCommandArgument('subreddit', sfCommandArgument::OPTIONAL, 'The name of any specific subreddit'),
+        ));
 
-    $this->namespace        = 'herddit';
-    $this->name             = 'create-episodes';
-    $this->briefDescription = 'Generates Episodes for all Subreddits without future Episodes.';
-    $this->detailedDescription = <<<EOF
+        $this->addOptions(array(
+            new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name'),
+            new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
+            new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'doctrine'),
+                // add your own options here
+        ));
+
+        $this->namespace = 'herddit';
+        $this->name = 'create-episodes';
+        $this->briefDescription = 'Generates Episodes for all Subreddits without future Episodes.';
+        $this->detailedDescription = <<<EOF
 The [herddit:create-episodes|INFO] task generates empty Episode objects for those Subreddits not having any future Episodes.  Calling with with a specific Subreddit name will generate Episode objects for only that Subreddit, assuming that it does not have future Episodes.
 Call it with:
 
   [php symfony herddit:create-episodes|INFO]
 EOF;
-  }
+    }
 
-  protected function execute($arguments = array(), $options = array())
-  {
-    // initialize the database connection
-    $databaseManager = new sfDatabaseManager($this->configuration);
-    $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
+    protected function execute($arguments = array(), $options = array())
+    {
+        // initialize the database connection
+        $databaseManager = new sfDatabaseManager($this->configuration);
+        $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
 
-    // add your code here
-    $subreddit = $arguments['subreddit'];
-    $searchByName = ($subreddit ? "subreddit.name = '$subreddit' AND" : "");
-    $mysql_query = "SELECT * FROM subreddit WHERE $searchByName id NOT IN
-        ( SELECT subreddit.id FROM subreddit
-        JOIN episode ON episode.subreddit_id = subreddit.id
-        GROUP BY (release_date)
-        HAVING (release_date > NOW()) );";
-    $stmt = $connection->prepare($mysql_query);
-    $stmt->execute();
-    $results = $stmt->fetchAll();
-    print_r($results);
+        // add your code here
+        include_once(sfConfig::get('sf_lib_dir') . '/vendor/cron.phar');
 
-  }
+        $subreddit_name = $arguments['subreddit'] != "" ? $arguments['subreddit']
+                    : '%';
+
+
+        $subquery = Doctrine_Query::create()
+                ->select('Subreddit.id')
+                ->from('Subreddit')
+                ->leftJoin('Episode')
+                ->groupBy('Episode.release_date')
+                ->having('Episode.release_date > NOW()');
+        $subreddits = @Doctrine_Query::create()
+                ->from('Subreddit')
+                ->where('Subreddit.name LIKE :name',
+                        array(':name' => $subreddit_name))
+                ->whereNotIn('Subreddit.id', $subquery)
+                ->execute();
+
+
+        foreach ($subreddits as $subreddit) {
+            $episode_schedule = Cron\CronExpression::factory($subreddit->getEpisodeScheduleCronFormatted());
+
+            $creation_schedule = Cron\CronExpression::factory($subreddit->getCreateNewEpisodesCronFormatted());
+
+            $stop_creating = $creation_schedule->getNextRunDate();
+
+            $episode_date = new DateTime(date('Y-m-d H:i:s', time()));
+            while ($episode_schedule->getNextRunDate($episode_date)->getTimestamp()
+            <= $stop_creating->getTimestamp()) {
+                $episode_date = $episode_schedule->getNextRunDate($episode_date);
+
+                $episode = new Episode();
+                $episode->setSubreddit($subreddit);
+                $episode->setReleaseDate($episode_date->format('Y-m-d H:i:s'));
+                $episode->save();
+            }
+        }
+    }
+
 }
