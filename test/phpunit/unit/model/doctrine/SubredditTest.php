6ad0f55bddf31ee2a07844b2bda06cbb6670a857
Tom Doggett <nocoolnametom@gmail.com>
2011-10-01 12:37:23 -0600

    First test file made.

---
+++ test/phpunit/unit/model/doctrine/SubredditTest.php
@@ -0,0 +1,17 @@
+<?php
+require_once dirname(__FILE__).'/../../../bootstrap/unit.php';
+
+class SubredditTest extends sfPHPUnitBaseTestCase
+{
+  public function testDefault()
+  {
+    $t = $this->getTest();
+
+    // lime-like assertions
+    //$t->diag('hello world');
+    //$t->ok(true, 'test something');
+		
+    // native assertions
+    //$this->assertTrue(true, 'test something')
+  }
+}
