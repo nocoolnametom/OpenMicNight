f7e7ca939eb1504e0c685fad26704245b0173b17
Tom Doggett <nocoolnametom@gmail.com>
2011-10-24 15:42:33 -0600

    Overriding more generator files for the restGenerator so that I can check user credentials before taking any action.

---
+++ data/generator/sfDoctrineRestGenerator/default/parts/validateDelete.php
@@ -0,0 +1,9 @@
+  /**
+   * Applies the deletion validators to the payload posted to the service
+   *
+   * @param   string   $payload  A payload string
+   */
+  public function validateDelete($payload, sfWebRequest $request = null)
+  {
+    ;
+  }
