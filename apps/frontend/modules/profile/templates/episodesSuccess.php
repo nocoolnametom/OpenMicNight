757d60d8dac06e9ca7cb07d9314575ccb8c54dc1
Tom Doggett <nocoolnametom@gmail.com>
2011-12-06 17:57:46 -0700

    Changed date references in the profile Episode list page.  Need to add episodes awaiting approval to the page.

--- apps/frontend/modules/profile/templates/episodesSuccess.php
+++ apps/frontend/modules/profile/templates/episodesSuccess.php
@@ -1,17 +1,17 @@
 <h1>Current Episodes</h1>
 <?php foreach($current as $assignment): ?>
 <div class="subreddit_name">/r/<?php echo SubredditTable::getInstance()->find($assignment->getEpisode()->getSubredditId())->getDomain(); ?></div>
-<div class=""><?php echo link_to($assignment->getEpisode()->getReleaseDate(), 'episode/edit?id=' . $assignment->getEpisode()->getIncremented()); ?></div>
+<div class=""><?php echo link_to(date("g:ia, D j M Y", strtotime($assignment->getEpisode()->getReleaseDate())), 'episode/edit?id=' . $assignment->getEpisode()->getIncremented()); ?></div>
 <?php endforeach; ?>
 
-<h1>Upcoming Episodes</h1>
+<h1>Queued Episodes</h1>
 <?php foreach($future as $assignment): ?>
 <div class="subreddit_name">/r/<?php echo SubredditTable::getInstance()->find($assignment->getEpisode()->getSubredditId())->getDomain(); ?></div>
-<div class=""><?php echo $assignment->getEpisode()->getReleaseDate(); ?></div>
+<div class=""><?php echo date("g:ia, D j M Y", strtotime($assignment->getEpisode()->getReleaseDate())); ?></div>
 <?php endforeach; ?>
 
 <h1>Released Episodes</h1>
 <?php foreach($released as $assignment): ?>
 <div class="subreddit_name">/r/<?php echo SubredditTable::getInstance()->find($assignment->getEpisode()->getSubredditId())->getDomain(); ?></div>
-<div class=""><?php echo link_to($assignment->getEpisode()->getReleaseDate(), 'episode/show?id=' . $assignment->getEpisode()->getIncremented()); ?></div>
+<div class=""><?php echo link_to(date("g:ia, D j M Y", strtotime($assignment->getEpisode()->getReleaseDate())), 'episode/show?id=' . $assignment->getEpisode()->getIncremented()); ?></div>
 <?php endforeach; ?>
