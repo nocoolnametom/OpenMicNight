26909f13ecd44d9b1edc3d036bd933fb290e4954
Tom Doggett <nocoolnametom@gmail.com>
2011-12-30 13:26:45 -0700

    Incorrect image reference in slideshow code.

--- apps/frontend/modules/home/templates/_build_slideshow.php
+++ apps/frontend/modules/home/templates/_build_slideshow.php
@@ -1,9 +1,8 @@
 <?php if (!count($episodes)): ?>
-    <img src='http://openmicnight/images/gradient.jpg' />
+    <?php echo image_tag('gradient.jpg'); ?>
 <?php else: ?>
     <?php foreach ($episodes as $episode): ?>
         <a href="<?php echo url_for('@episode_show?id=' . $episode->getIncremented()) ?>" target="_blank">
-            <img src='http://openmicnight/uploads/graphics/12d3c9a4ad810a2b3cd350191cbe467c3735193b.jpg' />
             <?php
             $image = ($episode->getGraphicFile() && !$episode->getIsNsfw() ? '/'
                             . trim(str_replace(sfConfig::get('sf_web_dir'), '',
