c87e05a2fe7b1dadc8c2e06d9567280b0af70082
Tom Doggett <nocoolnametom@gmail.com>
2012-01-03 16:43:02 -0700

    Typo fixed in subreddit actions modules.

--- apps/frontend/modules/episode/templates/indexSuccess.php
+++ apps/frontend/modules/episode/templates/indexSuccess.php
@@ -13,7 +13,7 @@
   <tbody>
     <?php foreach ($episodes as $episode): ?>
     <tr>
-      <td><?php echo $subreddits[$episode->getSubredditId()]->getName() ?></td>
+      <td><?php echo (array_key_exists($episode->getSubredditId(), $subreddits) ? $subreddits[$episode->getSubredditId()]->getName() : '')?></td>
       <td><a href="<?php echo url_for('episode/show?id='.$episode->getId()) ?>"><?php echo $episode->getTitle() ?></a><?php echo ($episode->getIsNsfw() ? '<span if="nsfw">NSFW</span>' : '') ?></td>
       <td><?php echo $users[$assignments[$episode->getEpisodeAssignmentId()]->getSfGuardUserId()]->getUsername() ?></td>
       <td><?php echo ($episode->getRedditPostUrl() ? link_to($episode->getRedditPostUrl(), $episode->getRedditPostUrl()) : '') ?></td>
