856696dcda3d848e09f35078991eced19be1b41e
Tom Doggett <nocoolnametom@gmail.com>
2012-01-03 17:01:54 -0700

    QA with Ken.

--- apps/frontend/modules/subreddit/templates/_form.php
+++ apps/frontend/modules/subreddit/templates/_form.php
@@ -12,7 +12,7 @@
           <?php echo $form->renderHiddenFields(false) ?>
           &nbsp;<a href="<?php echo url_for('subreddit/index') ?>">Back to list</a>
           <?php if (!is_null($form->getObject()->getId())): ?>
-            &nbsp;<?php echo link_to('Delete', 'subreddit/delete?id='.$form->getObject()->getId(), array('method' => 'delete', 'confirm' => 'Are you sure?')) ?>
+            &nbsp;<?php echo ($sf_user->isSuperAdmin() ? link_to('Delete', 'subreddit/delete?id='.$form->getObject()->getId(), array('method' => 'delete', 'confirm' => 'Are you sure?')) : '') ?>
           <?php endif; ?>
           <input type="submit" value="Save" />
         </td>
