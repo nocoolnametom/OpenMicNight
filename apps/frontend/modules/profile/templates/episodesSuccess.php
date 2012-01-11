9173005c4ba085e83eb083a6e05ac7b97ef71001
Tom Doggett <nocoolnametom@gmail.com>
2012-01-11 16:48:52 -0700

    .

--- apps/frontend/modules/profile/templates/episodesSuccess.php
+++ apps/frontend/modules/profile/templates/episodesSuccess.php
@@ -7,60 +7,66 @@
     <?php foreach ($approvals as $episode): ?>
         <div class="subreddit_name">/r/<?php echo $subreddits[$episode->getSubredditId()]->getDomain(); ?></div>
         <div class=""><?php
-        echo link_to(date("g:ia, D j M Y", strtotime($episode->getReleaseDate())),
-                                                     'episode/approval?id=' . $episode->getIncremented());
-
+        echo link_to(date("g:ia, D j M Y", strtotime($episode->getReleaseDate())), 'episode/approval?id=' . $episode->getIncremented());
         ?></div>
-    <?php endforeach; ?>
-<?php endif; ?>
+        <?php endforeach; ?>
+    <?php endif; ?>
 
 <?php if (count($current)): ?>
     <h2>Current Episodes</h2>
-        <?php foreach ($current as $assignment): ?>
-        <div class="subreddit_name">/r/<?php echo $subreddits[$assignment->getEpisode()->getSubredditId()]->getDomain(); ?></div>
-        <div class=""><?php
-        echo link_to(date("g:ia, D j M Y",
-                          strtotime($assignment->getEpisode()->getReleaseDate())),
-                                    'episode/edit?id=' . $assignment->getEpisode()->getIncremented());
+    <?php foreach ($current as $assignment): ?>
+        <div class="episode">
+            <span class="subreddit_name">/r/<?php echo $subreddits[$assignment->getEpisode()->getSubredditId()]->getDomain(); ?></span>
+            <span class="release_date"><?php
+        echo link_to(date("g:ia, D j M Y", strtotime($assignment->getEpisode()->getReleaseDate())), 'episode/edit?id=' . $assignment->getEpisode()->getIncremented());
+        ?></span>
+            <span class="deadline"><?php
+        $release_date = strtotime($assignment->getEpisode()->getReleaseDate());
+        $deadline_seconds = $deadline_rules_array[$assignment->getEpisode()->getSubredditId()][$assignment->getAuthorTypeId()];
+        echo date("g:ia, D j M Y", $release_date - $deadline_seconds);
+        ?></span>
+        </div>
+            <?php endforeach; ?>
+        <?php endif; ?>
 
-        ?></div>
-    <?php endforeach; ?>
-<?php endif; ?>
-
-    <?php if (count($future)): ?>
+        <?php if (count($future)): ?>
     <h2>Queued Episodes</h2>
     <?php foreach ($future as $assignment): ?>
-        <div class="subreddit_name">/r/<?php echo $subreddits[$assignment->getEpisode()->getSubredditId()]->getDomain(); ?></div>
-        <div class=""><?php echo date("g:ia, D j M Y",
-                                                                                                                                                                                                                                                                                                                                     strtotime($assignment->getEpisode()->getReleaseDate()));
-
-        ?></div>
-        <?php endforeach; ?>
-    <?php endif; ?>
-
-<?php if (count($released)): ?>
-    <h2>Released Episodes</h2>
-        <?php foreach ($released as $assignment): ?>
-        <div class="subreddit_name">/r/<?php echo $subreddits[$assignment->getEpisode()->getSubredditId()]->getDomain(); ?></div>
-        <div class=""><?php
-        echo link_to(date("g:ia, D j M Y",
-                          strtotime($assignment->getEpisode()->getReleaseDate())),
-                                    'episode/show?id=' . $assignment->getEpisode()->getIncremented());
-
-            ?></div>
-        <?php endforeach; ?>
-    <?php if (!(($page == 1 || $page == 0) && count($released) == 0)): ?>
-        <div class="navigation"> view more: 
-        <?php if ($page > 1): ?>
-                <a href="<?php echo url_for('profile/episodes?page=' . ($page - 1)) ?>">prev</a>
+        <div class="episode">
+            <span class="subreddit_name">/r/<?php echo $subreddits[$assignment->getEpisode()->getSubredditId()]->getDomain(); ?></span>
+            <span class=""><?php
+        echo date("g:ia, D j M Y", strtotime($assignment->getEpisode()->getReleaseDate()));
+        ?></span>
+            <span class="deadline"><?php
+        $release_date = strtotime($assignment->getEpisode()->getReleaseDate());
+        $deadline_seconds = $deadline_rules_array[$assignment->getEpisode()->getSubredditId()][$assignment->getAuthorTypeId()];
+        echo date("g:ia, D j M Y", $release_date - $deadline_seconds);
+        ?></span>
+        </div>
+            <?php endforeach; ?>
         <?php endif; ?>
-        <?php echo (($page > 1 && count($released) > 0)
-                    ? ' | ' : '');
 
-        ?>
+        <?php if (count($released)): ?>
+    <h2>Released Episodes</h2>
+    <?php foreach ($released as $assignment): ?>
+        <div class="episode">
+            <span class="subreddit_name">/r/<?php echo $subreddits[$assignment->getEpisode()->getSubredditId()]->getDomain(); ?></span>
+            <span class=""><?php
+        echo link_to(date("g:ia, D j M Y", strtotime($assignment->getEpisode()->getReleaseDate())), 'episode/show?id=' . $assignment->getEpisode()->getIncremented());
+        ?></span>
+        </div>
+            <?php endforeach; ?>
+            <?php if (!(($page == 1 || $page == 0) && count($released) == 0)): ?>
+        <div class="navigation"> view more: 
+                <?php if ($page > 1): ?>
+                <a href="<?php echo url_for('profile/episodes?page=' . ($page - 1)) ?>">prev</a>
+                <?php endif; ?>
+                <?php
+                echo (($page > 1 && count($released) > 0) ? ' | ' : '');
+                ?>
         <?php if (count($released) > 0): ?>
                 <a href="<?php echo url_for('subreddit/episodes?page=' . ($page + 1)) ?>">next</a>
-        <?php endif; ?>
+            <?php endif; ?>
         </div>
+        <?php endif; ?>
     <?php endif; ?>
-<?php endif; ?>
