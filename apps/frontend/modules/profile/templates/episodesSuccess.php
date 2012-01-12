d1823a90b9728bed7a9cc570553d18874ef496d0
Tom Doggett <nocoolnametom@gmail.com>
2012-01-11 17:12:40 -0700

    Fixed an error where the deadlines for current episodes way not showing up.

--- apps/frontend/modules/profile/templates/episodesSuccess.php
+++ apps/frontend/modules/profile/templates/episodesSuccess.php
@@ -10,8 +10,8 @@
         <div class=""><?php
         echo link_to(date("g:ia, D j M Y", strtotime($episode->getReleaseDate())), 'episode/approval?id=' . $episode->getIncremented());
         ?></div>
-        <?php endforeach; ?>
-    <?php endif; ?>
+    <?php endforeach; ?>
+<?php endif; ?>
 
 <?php if (count($current)): ?>
     <h2>Current Episodes</h2>
@@ -22,15 +22,18 @@
         echo link_to(date("g:ia, D j M Y", strtotime($assignment->getEpisode()->getReleaseDate())), 'episode/edit?id=' . $assignment->getEpisode()->getIncremented());
         ?></span>
             <span class="deadline"><?php
-        $release_date = strtotime($assignment->getEpisode()->getReleaseDate());
-        $deadline_seconds = $deadlines[$assignment->getEpisode()->getSubredditId()][$assignment->getAuthorTypeId()];
-        echo date("g:ia, D j M Y", $release_date - $deadline_seconds);
+        if (array_key_exists($assignment->getEpisode()->getSubredditId(), $deadlines)
+                && array_key_exists($assignment->getAuthorTypeId(), $deadlines[$assignment->getEpisode()->getSubredditId()])) {
+            $release_date = strtotime($assignment->getEpisode()->getReleaseDate());
+            $deadline_seconds = $deadlines[$assignment->getEpisode()->getSubredditId()][$assignment->getAuthorTypeId()];
+            echo date("g:ia, D j M Y", $release_date - $deadline_seconds);
+        } else { echo 'invalid episode assignment' ; }
         ?></span>
         </div>
-            <?php endforeach; ?>
-        <?php endif; ?>
+    <?php endforeach; ?>
+<?php endif; ?>
 
-        <?php if (count($future)): ?>
+<?php if (count($future)): ?>
     <h2>Queued Episodes</h2>
     <?php foreach ($future as $assignment): ?>
         <div class="episode">
@@ -39,16 +42,18 @@
         echo date("g:ia, D j M Y", strtotime($assignment->getEpisode()->getReleaseDate()));
         ?></span>
             <span class="deadline"><?php
-        $release_date = strtotime($assignment->getEpisode()->getReleaseDate());
-        die(var_dump($deadlines[$assignment->getEpisode()->getSubredditId()]) . var_dump($assignment->getAuthorTypeId()));
-        $deadline_seconds = $deadlines[$assignment->getEpisode()->getSubredditId()][$assignment->getAuthorTypeId()];
-        echo date("g:ia, D j M Y", $release_date - $deadline_seconds);
+        if (array_key_exists($assignment->getEpisode()->getSubredditId(), $deadlines)
+                && array_key_exists($assignment->getAuthorTypeId(), $deadlines[$assignment->getEpisode()->getSubredditId()])) {
+            $release_date = strtotime($assignment->getEpisode()->getReleaseDate());
+            $deadline_seconds = $deadlines[$assignment->getEpisode()->getSubredditId()][$assignment->getAuthorTypeId()];
+            echo date("g:ia, D j M Y", $release_date - $deadline_seconds);
+        } else { echo 'invalid episode assignment' ; }
         ?></span>
         </div>
             <?php endforeach; ?>
-        <?php endif; ?>
+<?php endif; ?>
 
-        <?php if (count($released)): ?>
+<?php if (count($released)): ?>
     <h2>Released Episodes</h2>
     <?php foreach ($released as $assignment): ?>
         <div class="episode">
@@ -58,17 +63,17 @@
         ?></span>
         </div>
             <?php endforeach; ?>
-            <?php if (!(($page == 1 || $page == 0) && count($released) == 0)): ?>
+    <?php if (!(($page == 1 || $page == 0) && count($released) == 0)): ?>
         <div class="navigation"> view more: 
-                <?php if ($page > 1): ?>
+        <?php if ($page > 1): ?>
                 <a href="<?php echo url_for('profile/episodes?page=' . ($page - 1)) ?>">prev</a>
-                <?php endif; ?>
-                <?php
-                echo (($page > 1 && count($released) > 0) ? ' | ' : '');
-                ?>
-        <?php if (count($released) > 0): ?>
+            <?php endif; ?>
+            <?php
+            echo (($page > 1 && count($released) > 0) ? ' | ' : '');
+            ?>
+            <?php if (count($released) > 0): ?>
                 <a href="<?php echo url_for('subreddit/episodes?page=' . ($page + 1)) ?>">next</a>
             <?php endif; ?>
         </div>
         <?php endif; ?>
-    <?php endif; ?>
+<?php endif; ?>
