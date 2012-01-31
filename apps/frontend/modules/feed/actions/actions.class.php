b760865effca53794747de4cb15f5a488abbe1c1
Tom Doggett <nocoolnametom@gmail.com>
2012-01-31 10:37:03 -0700

    Getting the defualt subreddit feed to work.

--- apps/frontend/modules/feed/actions/actions.class.php
+++ apps/frontend/modules/feed/actions/actions.class.php
@@ -338,7 +338,7 @@ class feedActions extends sfActions
                 'is_nsfw' => ($episode->getIsNsfw() ? 'yes' : 'no'),
             );
             if ($episode->getGraphicFile())
-                $new_entry['thumbnail'] = 'http://' . rtrim (ProjectConfiguration::getApplicationAmazonCloudFrontUrl(), '/') . '/upload/' . $episode->getGraphicFile();
+                $new_entry['thumbnail'] = rtrim (ProjectConfiguration::getApplicationAmazonCloudFrontUrl(), '/') . '/upload/' . $episode->getGraphicFile();
             $feedArray['entries'][] = $new_entry;
         }
 
